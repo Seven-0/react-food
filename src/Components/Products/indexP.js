7379bcad464fecec0c20b9c13c846c198d5dd90a
DESKTOP-26C85UL\HP <sevensimanjuntak21@gmail.com>
2022-01-05 17:18:01 +0700

    Fixed Map

--- src/Components/Products/indexP.js
+++ src/Components/Products/indexP.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import ProductContainer from "./ProductContainer";
+
 // import { Row, Card, Col } from "react-bootstrap";
 import {
   ProductsContainer,
   ProductWrapper,
   ProductsHeading,
-  ProductTitle,
-  ProductCard,
-  ProductImg,
-  ProductInfo,
-  ProductDesc,
-  ProductPrice,
+  // ProductTitle,
+  // ProductCard,
+  // ProductImg,
+  // ProductInfo,
+  // ProductDesc,
+  // ProductPrice,
   // ProductButton,
 } from "./ProductsElements";
 
-const Products = ({heading}) => {
-  const [shops, setShops] = useState([]);
+const Products = ({ heading }) => {
+  const [data, setShops] = useState([]);
 
   useEffect(() => {
     axios
@@ -24,59 +26,40 @@ const Products = ({heading}) => {
       )
       .then(async (Response) => {
         console.log("Respone:", Response.data.payload);
-
-        setShops(Response.data);
+        setShops(Response.data.payload);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  // const Products = ({heading}) => {
-  //   const [shops, setMakanans] = useState([]);
-  //   useEffect(() => {
-  //     fectData();
-  //   }, []);
-
-  //   const fectData = async () => {
-  //     const url =
-  //       "http://midas-be-task.herokuapp.com/midas/config/v1/foodDelivery/dataContent?version=1";
-  //     const response = await fetch(url);
-  //     const data = await response.json();
-  //     setMakanans(data);
-  //     this.setMakanans({});
-  //     console.log(data);
-  //   };
- 
-
   return (
     <div>
       <ProductsContainer>
         <ProductsHeading> {heading} </ProductsHeading>
         <ProductWrapper>
-          {shops.map((makanan) => {
-            // .filter((makanan) => makanan.code === makanan)
+          {data.map((payload) => {          
             return (
-              <div>
-                {makanan.menus.map((menu) => (
-                  <ProductCard key={menu.code}>
-                    <ProductImg src={menu.image} />
-                    {console.log("lognya: ", menu.shopName)}
-                    <ProductInfo>
-                      <ProductTitle> {menu.shopName} </ProductTitle>
-                      <ProductDesc> {menu.ind} </ProductDesc>
-                      <ProductPrice> {"Rp. " + menu.price} </ProductPrice>
-                      {/* <ProductButton>{makanan.button} </ProductButton> */}
-                    </ProductInfo>
-                  </ProductCard>
-                ))}
-              </div>
-            );
+              payload.shop.map((shop) =>{
+                return(
+                  shop.Menus.map(menu => (
+                    <ProductContainer
+                    key={menu.code.toString()}
+                    img={menu.image}
+                    title={menu.title}
+                    desc={menu.ind}
+                    price={menu.price} 
+                    />
+
+                  )
+                ));
+
+              }));
           })}
         </ProductWrapper>
       </ProductsContainer>
     </div>
   );
-}
+};
 
 export default Products;
